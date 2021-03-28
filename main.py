007259aa581fc60a5676f0b9656a787a8319acc2
riyanshbuktar <74132713+riyanshbuktar@users.noreply.github.com>
2021-03-28 18:05:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     st.title("Loan Approval Prediction")
     html_temp = """
     <div style="background-color:#dd88b3 ;padding:10px">
-    <h2 style="color:white;text-align:center;">Blood Donation Default Prediction App </h2>
+    <h2 style="color:white;text-align:center;">Loan Approval Prediction App </h2>
     </div>
     """    
     st.markdown(html_temp, unsafe_allow_html=True)
