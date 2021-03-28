83b803a6d63bc0d353ff9c0bcdbf36a42b763999
riyanshbuktar <74132713+riyanshbuktar@users.noreply.github.com>
2021-03-28 17:56:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Loading the saved Model
+model=pickle.load(open('model.pkl','rb'))
+
+
+def loan_default(features):
+
+    features = np.array(features).astype(np.float64).reshape(1,-1)
+    
+    predict = model.predict(features)
+    probability = model.predict_proba(features)
+
+    return predict, probability
+
+
+def main():
+    st.title("Loan Approval Prediction")
+    html_temp = """
+    <div style="background-color:#dd88b3 ;padding:10px">
+    <h2 style="color:white;text-align:center;">Blood Donation Default Prediction App </h2>
+    </div>
+    """    
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    Amount_Requested = st.text_input("Amount Requested")
+    Risk_Score = st.text_input("Frequency(times)")
+    Debt_to_Income_Ratio= st.text_input("Monetory(cc blood)")
+    Employment_Length= st.text_input("Time(months) ")
+
+
+
+    if st.button("Predict"):
+        
+        features = [Amount_Requested,Risk_Score,Debt_to_Income_Ratio,Employment_ssLength]
+        predict, proba = loan_default(features)
+        if predict[0] == 1:
+            
+
+            st.success('Congratulations ! Your loan request has been Approved {} %'.format(round(np.max(proba)*100),2))
+
+        else:
+            st.success('Sorry ! but your Loan Request is not approved{} %'.format(round(np.max(proba)*100),2))
+
+
+
+
+if __name__ == '__main__':
+    main()
