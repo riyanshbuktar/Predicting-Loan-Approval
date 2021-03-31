c62d1f08e8230df5a459dcdb97deb85d822b7a33
riyanshbuktar <74132713+riyanshbuktar@users.noreply.github.com>
2021-04-01 04:26:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
 # Loading the saved Model
